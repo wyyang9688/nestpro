0ffc472283c8d5658d66ff667fc28cbc45955ccf
wangyangyang <wyyang9688@qq.com>
2024-06-13 22:31:17 +0800

    静态资源配置

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { abortOnError: false });
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    abortOnError: false,
+  });
+  // app.useStaticAssets('public'); //配置静态资源http://localhost:3000/header.jpg
+  app.useStaticAssets('public', {
+    prefix: '/static/',
+  }); //http://localhost:3000/static/header.jpg
   await app.listen(3000);
 }
 bootstrap();
